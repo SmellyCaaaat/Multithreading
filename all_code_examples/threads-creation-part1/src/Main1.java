1dd6a6000f4a653d820abc735a67ca393806363d
SmellyCaaat <ethanxiaoma@gmail.com>
2020-02-15 18:23:50 -0800

    remove empty lines

--- all_code_examples/threads-creation-part1/src/Main1.java
+++ all_code_examples/threads-creation-part1/src/Main1.java
@@ -37,11 +37,8 @@ public class Main1 {
                 System.out.println("Current thread priority is " + Thread.currentThread().getPriority());
             }
         });
-
         thread.setName("New Worker Thread");
-
         thread.setPriority(Thread.MAX_PRIORITY);
-
         System.out.println("We are in thread: " + Thread.currentThread().getName()+ " before starting a new thread");
         thread.start();
         System.out.println("We are in thread: " + Thread.currentThread().getName()+ " after starting a new thread");
